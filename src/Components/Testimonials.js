7b92ab6c0d38d6528c2d8fb55be206d944982139
Jasmin Trotter <jasminmtrotter@gmail.com>
2019-04-11 16:14:32 -0700

    testimonials page and styles

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -1,9 +1,70 @@
 import React from 'react';
+import '../styles/Testimonials.css';
 
 export default function Testimonials(props) {
 	return(
 		<div className="Testimonials-container">
-          
+			<h2>Testimonials</h2>
+			<section className="testimonial-item">
+				<div className="quote-block">
+					<i class="fas fa-quote-left"></i>
+						<div>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+						</div>
+					<i class="fas fa-quote-right"></i>
+				</div>
+				<p className="attribution-block">
+					<em>-- Lorem Ipsum, Dolor Sit Amet Company</em>
+				</p>
+			</section>
+			<section className="testimonial-item">
+				<div className="quote-block">
+					<i class="fas fa-quote-left"></i>
+						<div>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+						</div>
+					<i class="fas fa-quote-right"></i>
+				</div>
+				<p className="attribution-block">
+					<em>-- Lorem Ipsum, Dolor Sit Amet Company</em>
+				</p>
+			</section>
+			<section className="testimonial-item">
+				<div className="quote-block">
+					<i class="fas fa-quote-left"></i>
+						<div>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+						</div>
+					<i class="fas fa-quote-right"></i>
+				</div>
+				<p className="attribution-block">
+					<em>-- Lorem Ipsum, Dolor Sit Amet Company</em>
+				</p>
+			</section>
+			<section className="testimonial-item">
+				<div className="quote-block">
+					<i class="fas fa-quote-left"></i>
+						<div>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+						</div>
+					<i class="fas fa-quote-right"></i>
+				</div>
+				<p className="attribution-block">
+					<em>-- Lorem Ipsum, Dolor Sit Amet Company</em>
+				</p>
+			</section>
+			<section className="testimonial-item">
+				<div className="quote-block">
+					<i class="fas fa-quote-left"></i>
+						<div>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+						</div>
+					<i class="fas fa-quote-right"></i>
+				</div>
+				<p className="attribution-block">
+					<em>-- Lorem Ipsum, Dolor Sit Amet Company</em>
+				</p>
+			</section>
         </div>
 	);
 }
